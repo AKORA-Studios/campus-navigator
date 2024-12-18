c09a50f6ff9dc5a9f895b176faa5b0a5c1579865
AKORA <65976562+A-K-O-R-A@users.noreply.github.com>
2024-12-18 22:31:13 +0100

    Extract image data class, add comments

--- lib/api/building/building.dart
+++ lib/api/building/building.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:ui' as ui;
 
 import 'package:campus_navigator/api/building/parsing/building_data.dart';
 import 'package:campus_navigator/api/building/parsing/common.dart';
@@ -8,16 +7,19 @@ import 'package:html/dom.dart';
 import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 
-import 'parsing/common.dart' as common;
+import 'page_image_data.dart';
 import 'parsing/layer_data.dart';
 import 'parsing/raum_bez_data.dart';
 import 'parsing/room_polygon.dart';
 
-// Matches variables that define a json object
+/// Matches variables that define a json object
 final RegExp variableDeclarationExp =
-    RegExp(r"^var (\w+) = ({[^;]+)", multiLine: true);
-final RegExp raumbezExp = RegExp(r"^raumbezData = ({[^;]+)", multiLine: true);
+    RegExp(r"^(var )?(\w+) = ({[^;]+)", multiLine: true);
+
+// Matches JS object attribute names
 final RegExp jsObjectExp = RegExp(r"(\w+):", multiLine: true);
+
+final RegExp raumbezExp = RegExp(r"^raumbezData = ({[^;]+)", multiLine: true);
 
 final RegExp pngFileNameExp =
     RegExp(r'var png_file_name = "(\w+)";', multiLine: true);
@@ -27,68 +29,6 @@ final RegExp stringVariableExp =
 
 final RegExp numberVariableExp =
     RegExp(r'(var)? ([\w_]+) = (\d+);', multiLine: true);
-
-class PageImageData {
-  final int qualiStep;
-  final Map<String, ui.Image> backgroundImages;
-
-  PageImageData({
-    required this.qualiStep,
-    required this.backgroundImages,
-  });
-
-  ui.Image? getLayerSymbol(String symbolName) {
-    return backgroundImages[symbolName];
-  }
-
-  ui.Image? getBackgroundImage(int x, int y) {
-    return backgroundImages["${x}_$y"];
-  }
-
-  /// Fetches all images for a floor plan given the `pngFileName` of
-  /// the current page and all it's layers(needed for fetching of the symbolds)
-  static Future<PageImageData> fetchLevelImages(
-      String pngFileName, List<LayerData> layers,
-      {int qualiIndex = 2}) async {
-    final List<int> qualiSteps = [1, 2, 4, 8];
-    final int qualiStep = qualiSteps[qualiIndex];
-
-    // Prepare buffer, this is to avoid concurrency bugs during fetching
-    List<Future<(String, ui.Image?)>> imageBuffer = [];
-
-    // Background tiles
-    for (int x = 0; x < qualiStep; x++) {
-      for (int y = 0; y < qualiStep; y++) {
-        final uri = Uri.parse(
-            "$baseURL/images/etplan_cache/${pngFileName}_$qualiStep/${x}_$y.png/nobase64");
-
-        final imageFuture =
-            common.fetchImage(uri).then((image) => ("${x}_$y", image));
-
-        imageBuffer.add(imageFuture);
-      }
-    }
-
-    // Layer symbols
-    for (final layer in layers) {
-      final imageFuture = common
-          .fetchImage(layer.getSymbolUri())
-          .then((image) => (layer.symbolPNG, image));
-
-      imageBuffer.add(imageFuture);
-    }
-
-    var imageList = await Future.wait(imageBuffer);
-
-    Map<String, ui.Image> imageMap = {};
-    for (final e in imageList) {
-      if (e.$2 == null) continue;
-      imageMap[e.$1] = e.$2!;
-    }
-
-    return PageImageData(backgroundImages: imageMap, qualiStep: qualiStep);
-  }
-}
 
 class RoomPage {
   final HTMLData htmlData;
@@ -118,26 +58,33 @@ class RoomPage {
 
     var buildingInfo = BuildingData.fromHTMLDocument(htmlData.document);
 
+    // Parse room labels
     final raumBezMatch = raumbezExp.firstMatch(htmlData.script)!;
-    final json = jsonDecode(raumBezMatch[1]!);
-    RaumBezData raumBezData = RaumBezData.fromJson(json);
+    RaumBezData raumBezData =
+        RaumBezData.fromJson(jsonDecode(raumBezMatch[1]!));
 
+    // Parse all variables with JSON object like values
     var matches = variableDeclarationExp.allMatches(htmlData.script);
     Map<String, dynamic> variables = {};
     for (final Match m in matches) {
-      String varName = m[1]!;
-      String varValue =
-          m[2]!.replaceAllMapped(jsObjectExp, (m) => '"' + m[1]! + '":');
-      var json = jsonDecode(varValue);
+      String varName = m[2]!;
+      String valueJsNotation = m[3]!;
 
-      variables[varName] = json;
+      // Convert javascript object notation to JSON object notation
+      // { a: 1 }  ->  { "a": 1 }
+      String valueJsonNotation = valueJsNotation.replaceAllMapped(
+          jsObjectExp, (m) => '"' + m[1]! + '":');
+
+      variables[varName] = jsonDecode(valueJsonNotation);
     }
 
+    // Parse symbol layers
     List<LayerData> layers = variables.entries
         .where((e) => e.key.startsWith("slayer"))
         .map((e) => LayerData.fromJson(e.value))
         .toList();
 
+    // Parse room polygons for drawing
     List<List<RoomPolygon>> rooms = variables.entries
         .where((e) => !e.key.startsWith("slayer"))
         .map((e) => RoomPolygon.fromJsonList(e.value))
@@ -160,6 +107,7 @@ class RoomPage {
       numberVariables[m[2]!] = val;
     }
 
+    // The file name for the background image
     String pngFileName = stringVariables["png_file_name"]!;
 
     return RoomPage(
@@ -175,7 +123,7 @@ class RoomPage {
   }
 
   static Future<RoomPage> fetchRoom(String query) async {
-    final uri = Uri.parse("${baseURL}/etplan/$query");
+    final uri = Uri.parse("$baseURL/etplan/$query");
 
     final response = await http.get(uri);
 

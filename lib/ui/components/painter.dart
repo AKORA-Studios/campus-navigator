74a71a95e89603008eb1069e39d9f538a8206a58
AKORA <65976562+A-K-O-R-A@users.noreply.github.com>
2025-01-14 11:05:04 +0100

    custom point in poly

--- lib/ui/components/painter.dart
+++ lib/ui/components/painter.dart
@@ -95,10 +95,7 @@ class MapPainter extends CustomPainter {
 
         // Mouse hover
         if (inverseMousePos != null) {
-          final mouseHover = PolygonUtil.containsLocation(
-              inverseMousePos.toCoords(),
-              mapped.map((p) => p.toCoords()).toList(),
-              false);
+          final mouseHover = Poly.isPointInPolygon(inverseMousePos, mapped);
           if (mouseHover) color = Colors.red;
         }
 
@@ -307,5 +304,64 @@ List<Offset> mapPositions(List<Position> rawPoints) {
 extension ToLatLng on Offset {
   LatLng toCoords() {
     return LatLng(dx, dy);
+  }
+}
+
+// Inspired by: https://github.com/molteo-engineering-team/point_in_polygon/blob/58e567221f5c225d301cb1c2d4871c0454e61723/lib/point_in_polygon.dart
+// https://en.wikipedia.org/wiki/Point_in_polygon
+class Poly {
+  /// Check if a Point [point] is inside a polygon representing by a List of Point [vertices]
+  /// by using a Ray-Casting algorithm
+  static bool isPointInPolygon(Offset point, List<Offset> vertices) {
+    int intersectCount = 0;
+    for (int i = 0; i < vertices.length; i += 1) {
+      final Offset vertB =
+          i == vertices.length - 1 ? vertices[0] : vertices[i + 1];
+      if (Poly.rayCastIntersect(point, vertices[i], vertB)) {
+        intersectCount += 1;
+      }
+    }
+    return (intersectCount % 2) == 0;
+  }
+
+  /// Ray-Casting algorithm implementation
+  /// Calculate whether a horizontal ray cast eastward from [point]
+  /// will intersect with the line between [vertA] and [vertB]
+  /// Refer to `https://en.wikipedia.org/wiki/Point_in_polygon` for more explanation
+  /// or the example comment bloc at the end of this file
+  static bool rayCastIntersect(Offset point, Offset vertA, Offset vertB) {
+    final double aY = vertA.dy;
+    final double bY = vertB.dy;
+    final double aX = vertA.dx;
+    final double bX = vertB.dx;
+    final double pY = point.dy;
+    final double pX = point.dx;
+
+    if ((aY > pY && bY > pY) || (aY < pY && bY < pY) || (aX < pX && bX < pX)) {
+      // The case where the ray does not possibly pass through the polygon edge,
+      // because both points A and B are above/below the line,
+      // or both are to the left/west of the starting point
+      // (as the line travels eastward into the polygon).
+      // Therefore we should not perform the check and simply return false.
+      // If we did not have this check we would get false positives.
+      return false;
+    }
+
+    // y = mx + b : Standard linear equation
+    // (y-b)/m = x : Formula to solve for x
+
+    // M is rise over run -> the slope or angle between vertices A and B.
+    final double m = (aY - bY) / (aX - bX);
+    // B is the Y-intercept of the line between vertices A and B
+    final double b = ((aX * -1) * m) + aY;
+    // We want to find the X location at which a flat horizontal ray at Y height
+    // of pY would intersect with the line between A and B.
+    // So we use our rearranged Y = MX+B, but we use pY as our Y value
+    final double x = (pY - b) / m;
+
+    // If the value of X
+    // (the x point at which the ray intersects the line created by points A and B)
+    // is "ahead" of the point's X value, then the ray can be said to intersect with the polygon.
+    return x > pX;
   }
 }

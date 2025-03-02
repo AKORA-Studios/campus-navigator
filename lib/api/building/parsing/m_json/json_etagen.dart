1ce62e332e6a785a14f377126d47fc91579952e2
AKORA <65976562+A-K-O-R-A@users.noreply.github.com>
2025-03-02 16:23:51 +0100

    add json m

---
+++ lib/api/building/parsing/m_json/json_etagen.dart
@@ -0,0 +1,126 @@
+import 'dart:convert';
+
+class JsonEtagen {
+  final String etage;
+  final double maxY;
+  final double maxX;
+  final String raumf;
+  final List<Typen> typen;
+
+  JsonEtagen({
+    required this.etage,
+    required this.maxY,
+    required this.maxX,
+    required this.raumf,
+    required this.typen,
+  });
+
+  factory JsonEtagen.fromRawJson(String str) =>
+      JsonEtagen.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory JsonEtagen.fromJson(Map<String, dynamic> json) => JsonEtagen(
+        etage: json["etage"],
+        maxY: json["maxY"]?.toDouble(),
+        maxX: json["maxX"]?.toDouble(),
+        raumf: json["raumf"],
+        typen: List<Typen>.from(json["typen"].map((x) => Typen.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "etage": etage,
+        "maxY": maxY,
+        "maxX": maxX,
+        "raumf": raumf,
+        "typen": List<dynamic>.from(typen.map((x) => x.toJson())),
+      };
+}
+
+class Typen {
+  final List<Rooms> rume;
+  final int typ;
+
+  Typen({
+    required this.rume,
+    required this.typ,
+  });
+
+  factory Typen.fromRawJson(String str) => Typen.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Typen.fromJson(Map<String, dynamic> json) => Typen(
+        rume: List<Rooms>.from(json["räume"].map((x) => Rooms.fromJson(x))),
+        typ: json["typ"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "räume": List<dynamic>.from(rume.map((x) => x.toJson())),
+        "typ": typ,
+      };
+}
+
+class Rooms {
+  final List<Punkt> punkte;
+  final double? namey;
+  final double? namex;
+  final String? name;
+  final String id;
+  final bool list;
+
+  Rooms({
+    required this.punkte,
+    this.namey,
+    this.namex,
+    this.name,
+    required this.id,
+    required this.list,
+  });
+
+  factory Rooms.fromRawJson(String str) => Rooms.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Rooms.fromJson(Map<String, dynamic> json) => Rooms(
+        punkte: List<Punkt>.from(json["punkte"].map((x) => Punkt.fromJson(x))),
+        namey: json["namey"]?.toDouble(),
+        namex: json["namex"]?.toDouble(),
+        name: json["name"],
+        id: json["id"],
+        list: json["list"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "punkte": List<dynamic>.from(punkte.map((x) => x.toJson())),
+        "namey": namey,
+        "namex": namex,
+        "name": name,
+        "id": id,
+        "list": list,
+      };
+}
+
+class Punkt {
+  final double x;
+  final double y;
+
+  Punkt({
+    required this.x,
+    required this.y,
+  });
+
+  factory Punkt.fromRawJson(String str) => Punkt.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Punkt.fromJson(Map<String, dynamic> json) => Punkt(
+        x: json["x"]?.toDouble(),
+        y: json["y"]?.toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "x": x,
+        "y": y,
+      };
+}
